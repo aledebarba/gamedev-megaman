42be63912d69e5ca15ed506afc0892a8e078cbf5
logonrm <logonrm@fiap.com.br>
2018-02-05 22:48:48 -0200

    Primeiro Comit

---
+++ Assets/Script/Blink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+
+	SpriteRenderer spriteRender;
+	public float intervalo;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (intervalo);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (intervalo);
+
+		StartCoroutine (Start ());
+	}
+
+	void UpDate(){
+	
+		// Pressionar enter
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			SceneManager.LoadScene ("gamescene");
+		}
+	
+	}
+
+
+}
